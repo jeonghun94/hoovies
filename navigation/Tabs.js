7321821dfccc8bdec5599279e5ee6e03187233cf
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-05-17 17:41:07 +0900

    hoovies init

---
+++ navigation/Tabs.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movie from "../screens/Movie";
+import Search from "../screens/Search";
+import Tv from "../screens/Tv";
+import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  // const isDark = useColorScheme() === "dark";
+  // console.log(isDark);
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Tv"
+      // screenOptions={{
+      //   tabBarStyle: {
+      //     backgroundColor: isDark ? "#000" : "#fff",
+      //   },
+      //   headerStyle: {
+      //     backgroundColor: isDark ? "#000" : "#fff",
+      //   },
+      //   headerTitleStyle: {
+      //     color: isDark ? "#fff" : "#000",
+      //   },
+      // }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movie}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={"film-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tv"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+          tabBarBadge: 3,
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={"search-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
---
+++ screens/Movie.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Movie = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "white" }}>Movie</Text>
+    </View>
+  );
+};
+
+export default Movie;
